9fe5108a964e76cff147503b35633bdecd02dc46
Atanur <a.demir@azerion.com>
2022-01-28 03:51:56 +0300

    Eslint vue plugins added

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,10 @@ module.exports = {
     node: true,
   },
   extends: [
+    "plugin:vue/base",
     "plugin:vue/vue3-essential",
+    "plugin:vue/vue3-strongly-recommended",
+    "plugin:vue/vue3-recommended",
     "eslint:recommended",
     "plugin:prettier/recommended",
   ],
